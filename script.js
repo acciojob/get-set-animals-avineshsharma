4c2b4962626fc201ea729efffd657213fb092891
Avinesh sharma <142591660+avineshsharma@users.noreply.github.com>
2025-10-29 19:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
